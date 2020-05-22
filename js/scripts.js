eb954b26b163c5668f1603175292e5f3100393ad
samwel mwaura <smmuiruri@gmail.com>
2020-05-22 01:06:00 -0700

    Add git repo

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var modulo = function(number1, number2) {
+    return number1 % number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(modulo(number1, number2));
